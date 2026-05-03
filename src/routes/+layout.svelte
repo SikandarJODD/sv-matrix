d29e03bee2cc54327912d920a6a6132509c96c4f
SikandarJODD <sikandar6174@gmail.com>
2026-05-03 16:01:03 +0530

    feat: added runed library, base theme setup, base dotmatrix-loader.css file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { activeElement, PressedKeys } from 'runed';
+	import { ModeWatcher, toggleMode } from 'mode-watcher';
 
 	let { children } = $props();
+	let keys = new PressedKeys();
+
+	keys.onKeys(['d'], () => {
+		if (
+			activeElement.current?.localName === 'input' ||
+			activeElement.current?.localName === 'textarea'
+		)
+			return;
+		toggleMode();
+	});
 </script>
+
+<ModeWatcher defaultMode="dark" />
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
